ad2f690b8c77d032a34d06ef797a14afed94b89e
Dmitry Yakunin <zeil@yandex-team.ru>
2020-06-20 18:30:51 +0300

    BACKPORT: tcp: Expose tcp_sock_set_keepidle_locked
    This is preparation for usage in bpf_setsockopt.
    
    v2:
      - remove redundant EXPORT_SYMBOL (Alexei Starovoitov)
    
    Change-Id: Ie696e9bcc4d92c21210e07cdc35cfb9acb48303c
    Signed-off-by: Dmitry Yakunin <zeil@yandex-team.ru>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20200620153052.9439-2-zeil@yandex-team.ru

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -2830,7 +2830,7 @@ static void tcp_enable_tx_delay(void)
 	}
 }
 
-static int __tcp_sock_set_keepidle(struct sock *sk, int val)
+int tcp_sock_set_keepidle_locked(struct sock *sk, int val)
 {
 	struct tcp_sock *tp = tcp_sk(sk);
 
@@ -2857,7 +2857,7 @@ int tcp_sock_set_keepidle(struct sock *sk, int val)
 	int err;
 
 	lock_sock(sk);
-	err = __tcp_sock_set_keepidle(sk, val);
+	err = tcp_sock_set_keepidle_locked(sk, val);
 	release_sock(sk);
 	return err;
 }
@@ -3070,7 +3070,7 @@ static int do_tcp_setsockopt(struct sock *sk, int level,
 		break;
 
 	case TCP_KEEPIDLE:
-		err = __tcp_sock_set_keepidle(sk, val);
+		err = tcp_sock_set_keepidle_locked(sk, val);
 		break;
 	case TCP_KEEPINTVL:
 		if (val < 1 || val > MAX_TCP_KEEPINTVL)
